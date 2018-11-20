425799ecfca1afe31ddd1c9ec9925e1a65dbe137
Alina Moskalenko <alina.kravchenko@ukr.net>
2018-11-20 13:13:31 +0200

    Updates

---
+++ precache-manifest.3c7d0ee8505843115e36dcad6bae044d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "c674af9680f03f9f0890",
+    "url": "/test/static/js/main.c674af96.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/test/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "c674af9680f03f9f0890",
+    "url": "/test/static/css/main.d1b37e44.chunk.css"
+  },
+  {
+    "revision": "204f16d791a4a0e791fc8526ecadebb6",
+    "url": "/test/index.html"
+  }
+];
